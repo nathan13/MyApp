c861f46b492bf284e70bdee0b0305847a6380274
nathan13 <nathan.santana.s13@gmail.com>
2021-08-05 15:31:21 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const Sequelize = require('sequelize')
+const handlebars = require('express-handlebars')
+
+//config
+ // template engine
+  app.engine('handlebars',handlebars({defaultLayout:'main'}))
+  app.set('view engine', 'handlebars')
+  //database connection
+  const sequelize = new Sequelize('express','root','1234',{
+    host:"localhost",
+    dialect:'mysql'
+  })
+  // authenticating connection
+  sequelize.authenticate().then(function(){
+    console.log("Banco ok")
+  }).catch(function(erro){
+    console.log('Banco erro: '+erro)
+  })
+
+//routes
+app.get('/',function(req,res){
+   res.sendFile(__dirname+'/html/index.html')
+})
+app.get('/cad',function(req,res){
+  res.render('formulario')
+})
+app.post('/addcad',function(req,res){
+  res.send('postagem adicionada!')
+})
+//port express
+app.listen(8081, function() {
+  console.log("Server Ok")
+})
