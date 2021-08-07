1823c5eb4dcb0a23e1ab0927e74f924fa155f6cc
nathan13 <nathan.santana.s13@gmail.com>
2021-08-06 22:35:13 -0300

    Teste new branch

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const handlebars = require('express-handlebars')
   })
   // authenticating connection
   sequelize.authenticate().then(function(){
-    console.log("Banco ok")
+    console.log("Database ok")
   }).catch(function(erro){
-    console.log('Banco erro: '+erro)
+    console.log('Database erro: '+erro)
   })
 
 //routes
